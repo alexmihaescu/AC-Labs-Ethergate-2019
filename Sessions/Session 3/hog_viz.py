d9752e4044219db45cbbb844bed38938c66745b1
alexmihaescu <alexmihaescu@coreai.ai>
2020-04-16 14:42:26 +0300

    Display original image

--- Sessions/Session 3/hog_viz.py
+++ Sessions/Session 3/hog_viz.py
@@ -8,13 +8,14 @@ while cap.isOpened():
 	if not ret:
 		break
 
-	(H, hogImage) = feature.hog(image, orientations=3, pixels_per_cell=(16, 16),
-		cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1",
+	(H, hogImage) = feature.hog(image, orientations=7, pixels_per_cell=(16, 16),
+		cells_per_block=(2, 2), transform_sqrt=True, block_norm="L2",
 		visualize=True)
 	hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
 	hogImage = hogImage.astype("uint8")
 	
 	cv2.imshow("HOG VIZ", hogImage)
+	cv2.imshow('Input', image)
 
 	if cv2.waitKey(40) == 27:
 		break
